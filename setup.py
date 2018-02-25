da4a8c6f95ba93f73c28fca8704969216e1b1393
Maureen Aubrey <34176330+maubrey45@users.noreply.github.com>
2018-02-25 08:06:53 -0700

    Add files via upload
    Test of the Flask website and how does one code with it.

--- setup.py
+++ setup.py
@@ -3,20 +3,26 @@ import sys
 import Adafruit_DHT
 import datetime
 import time
-i = 0;
-listData = [];
-while i < 10:
+from flask import Flask
 
-    time.sleep(5);
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4);
-    t = datetime.datetime.now();
-    currentday = t.today();
-    tempString = 'Temp: {} C  Humidity: {} %  Date/time: {}'.format(temperature, humidity, t);
-    listData.append(tempString);
-    print tempString;
-    i = i+1;
-file = open("OutputFile.txt", 'a+');
-for x in range(0, len(listData)):
-	file.write(listData[x]);
-	file.write("\n");
-file.close();
+@app.route("/")
+def grabExternalData():
+	i = 0;
+	listData = [];
+	while i < 10:
+
+	    time.sleep(5);
+	    humidity, temperature = Adafruit_DHT.read_retry(11, 4);
+	    t = datetime.datetime.now();
+	    currentday = t.today();
+	    tempString = 'Temp: {} C  Humidity: {} %  Date/time: {}'.format(temperature, humidity, t);
+	    listData.append(tempString);
+	    print tempString;
+	    i = i+1; 
+	file = open("OutputFile.txt", 'a+');
+	for x in range(0, len(listData)):
+		file.write(listData[x]);
+		file.write("\n");
+	file.close();
+if __name__ == "__main__":
+    app.run()
