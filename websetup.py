43270742a7254af30ada9eea1471fa13e07ba57a
festivefish <jonasmatt19@gmail.com>
2018-02-25 09:28:14 -0700

    Create websetup.py
    setup file for exporting current data to a non-updating webpage

---
+++ websetup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+import datetime
+import time
+from flask import Flask
+
+@app.route("/")
+def grabExternalData():
+	    humidity, temperature = Adafruit_DHT.read_retry(11, 4);
+	    t = datetime.datetime.now();
+	    currentday = t.today();
+	    tempString = 'Temp: {} C  Humidity: {} %  Date/time: {}'.format(temperature, humidity, t);
+	    return tempString;
+	    
+if __name__ == "__main__":
+    app.run()
