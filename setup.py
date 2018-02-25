6f3be4ccbfaa347da096ead6ad076817ee1a2240
festivefish <jonasmatt19@gmail.com>
2018-02-25 11:30:08 -0700

    Update setup.py
    reverting flask implementation for text file output

--- setup.py
+++ setup.py
@@ -9,23 +9,22 @@ import time
 
 #@app.route("/")
 #def grabExternalData():
-	i = 0;
-	listData = [];
-	while i < 10:
-
-	    time.sleep(5);
-	    humidity, temperature = Adafruit_DHT.read_retry(11, 4);
-	    t = datetime.datetime.now();
-	    currentday = t.today();
-	    tempString = 'Temp: {} C  Humidity: {} %  Date/time: {}'.format(temperature, humidity, t);
-	    listData.append(tempString);
-	    print tempString;
-	    i = i+1; 
-	file = open("OutputFile.txt", 'a+');
-	for x in range(0, len(listData)):
-		file.write(listData[x]);
-		file.write("\n");
-	file.close();
+i = 0;
+listData = [];
+while i < 10:
+    time.sleep(5);
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4);
+    t = datetime.datetime.now();
+    currentday = t.today();
+    tempString = 'Temp: {} C  Humidity: {} %  Date/time: {}'.format(temperature, humidity, t);
+    listData.append(tempString);
+    print tempString;
+    i = i+1; 
+file = open("OutputFile.txt", 'a+');
+for x in range(0, len(listData)):
+	file.write(listData[x]);
+	file.write("\n");
+file.close();
 #if __name__ == "__main__":
 	
 	#app.run()
