c92cbd90dfff7894431b05de70fc2d9ad5d1992c
festivefish <jonasmatt19@gmail.com>
2018-02-25 10:25:40 -0700

    Update nothello.py

--- nothello.py
+++ nothello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    strout =  websetup.grabExternalData()
+    strout =  "asakdlaksjdal" + websetup.grabExternalData()
     return strout
 
 if __name__ == "__main__":
