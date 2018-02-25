e55ebcddd0f5377c457b7a5124045f515b6db704
Maureen Aubrey <34176330+maubrey45@users.noreply.github.com>
2018-02-24 19:50:38 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 import sys
 import Adafruit_DHT
 import datetime
+import time
 i = 0;
 while i < 10:
 
+    time.sleep(30);
     humidity, temperature = Adafruit_DHT.read_retry(11, 4);
     time = datetime.datetime.now();
-    currentday = time.day
+    currentday = time.day + 
     print 'Temp: {} C  Humidity: {} %  Date/time: {}'.format(temperature, humidity, currentday);
     i = i+1;
